a633208bb8c737a2cbd8abb771315c21230a67d6
Saksham27 <geekysaksham27@gmail.com>
2020-04-15 03:35:02 +0530

    Added: Read all records and store them in list

--- CSV.cs
+++ CSV.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
 
 namespace OpenCSV
 {
-    class Program
+    class CSV
     {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // read csv file
+            using (CsvReader csvReader = new CsvReader(new StreamReader(@"C:\Users\Saksham\source\repos\OpenCSV\StateCensusData.csv"), true))
+            {
+                // getting no of fields
+                int fieldsCount = csvReader.FieldCount;
+
+                // reading the headers in array
+                string[] headers = csvReader.GetFieldHeaders();
+
+                string[] recordsInARow = new string[fieldsCount];
+
+                List<string[]> records = new List<string[]>();
+
+                // adding all rows to list
+                while (csvReader.ReadNextRecord() != false)
+                {
+                    csvReader.CopyCurrentRecordTo(recordsInARow);
+                    records.Add(recordsInARow);
+                    Console.WriteLine(recordsInARow);
+                }
+            }
         }
     }
 }
