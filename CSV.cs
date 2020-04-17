6b8a2b912fbc758e105bb6e8e6ca261fa6274bab
Saksham27 <geekysaksham27@gmail.com>
2020-04-17 11:35:23 +0530

    Added: reading data from csv and string it to an arraylist

--- CSV.cs
+++ CSV.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using LumenWorks.Framework.IO.Csv;
 
 namespace OpenCSV
@@ -14,27 +15,47 @@ namespace OpenCSV
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // read csv file
-            using (CsvReader csvReader = new CsvReader(new StreamReader(@"C:\Users\Saksham\source\repos\OpenCSV\StateCensusData.csv"), true))
+
+            try
             {
-                // getting no of fields
-                int fieldsCount = csvReader.FieldCount;
-
-                // reading the headers in array
-                string[] headers = csvReader.GetFieldHeaders();
-
-                string[] recordsInARow = new string[fieldsCount];
-
-                List<string[]> records = new List<string[]>();
-
-                // adding all rows to list
-                while (csvReader.ReadNextRecord() != false)
+                // read csv file
+                using (CsvReader csvReader = new CsvReader(new StreamReader(File.OpenRead(@"C:\Users\Saksham\source\repos\OpenCSV\StateCensusData.csv")), true))
                 {
-                    csvReader.CopyCurrentRecordTo(recordsInARow);
-                    records.Add(recordsInARow);
-                    Console.WriteLine(recordsInARow);
+                    // getting no of fields
+                    int fieldsCount = csvReader.FieldCount;
+                    Console.WriteLine(fieldsCount);
+
+                    // reading the headers in array
+                    string[] headers = csvReader.GetFieldHeaders();
+
+                    //string[] recordsInARow = new string[fieldsCount];
+
+                    List<string[]> records = new List<string[]>();
+                    int i = 0;
+                    // adding all rows to list
+                    while (csvReader.ReadNextRecord())
+                    {
+                        string[] recordsInARow = new string[fieldsCount];
+                        
+                        csvReader.CopyCurrentRecordTo(recordsInARow);
+                        records.Add(recordsInARow);
+                        i++;       
+                    }
+                    // print the records
+                    foreach (string[] rec in records)
+                    {
+                        Console.WriteLine(String.Join(" ", rec));
+                    }
                 }
+            }
+
+            catch (FileNotFoundException file_not_found)
+            {
+                throw new Exception(file_not_found.FileName);
+            }
+
+
             }
         }
     }
-}
+
